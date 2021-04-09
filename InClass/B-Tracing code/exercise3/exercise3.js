aa93a8efdb01804ed00b83c81d2146b4baa575c8
Keti <k.palavandishvili@yahoo.com>
2021-04-10 01:02:46 +0200

    B => done

--- InClass/B-Tracing code/exercise3/exercise3.js
+++ InClass/B-Tracing code/exercise3/exercise3.js
@@ -1,24 +1,21 @@
-const getRepos = function(repoName) {
-  return fetch('https://api.github.com/users/' + repoName + '/repos')  
-    .then(data => data.json())
-    .then(function(response) {
-      return response.map(function(rep) {
-		console.log(rep.name);
+const getRepos = function (user) {
+  return fetch(`https://api.github.com/users/${user}/repos`)
+    .then((data) => data.json())
+    .then(function (response) {
+      return response.map(function (rep) {
+        var myElement = document.querySelector("#main");
+        var paragraph = document.createElement("p");
+        paragraph.innerText = rep.name;
+        myElement.appendChild(paragraph);
         return rep.name;
       });
     });
 };
 
-
-
-const migracodeRepos = getRepos('migracode-barcelona');
+const migracodeRepos = getRepos("migracode-barcelona");
 console.log("Loading...");
 
 
-var myElement = document.querySelector("#main");
-var paragraph = document.createElement("p"); 
-paragraph.innerText = migracodeRepos;
-myElement.appendChild(paragraph);
 
 //Task1. Fix the code
 //Task2. Create a <p> for each repository
