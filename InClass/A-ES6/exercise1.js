c72d0ff4b2a7cd5922a043d647f66d840d67f9fd
Keti <k.palavandishvili@yahoo.com>
2021-04-09 17:16:59 +0200

    InClass A-ES6 => done

--- InClass/A-ES6/exercise1.js
+++ InClass/A-ES6/exercise1.js
@@ -1,25 +1,39 @@
 // Turn this function into an arrow function
-function arrow() {
-  return "es6 is awesome!";
-}
+// function arrow() {
+//   return "es6 is awesome!";
+// }
+const arrow = () => "es6 is awesome!";
+console.log("es6 is awesome!");
+
 
 //Use the es6 syntax for default parameter
-function defaultParameter(name) {
-  var name = name || "sam";
-  return name;
-}
+// function defaultParameter(name) {
+//   var name = name || "sam";
+//   return name;
+// }
+const defaultParameter = (name = "sam") => name;
+console.log("sam");
+
 
 // Use the spread operator to combine arr1 and arr2
-function combineArrays(arr1, arr2) {
-  return arr1.concat(arr2);
-}
+// function combineArrays(arr1, arr2) {
+//   return arr1.concat(arr2);
+// }
+const combineArrays = (arr1, arr2) => [...arr1, ...arr2] ;
+console.log("this is spread operator");
 
 //use destructuring to return the object's cyf property
-function destructuring(obj) {
-  return obj.cyf;
+// function destructuring(obj) {
+//   return obj.cyf;
+// }
+const destructuring = (obj) => {
+  const { cyf } = obj;
 }
+console.log("this returns object's cyf property");
 
 // use template literal to return a string with the sum of a and b
-function templateString(a, b) {
-  return "The sum is equal to " + (a + b).toString();
-}
+// function templateString(a, b) {
+//   return "The sum is equal to " + (a + b).toString();
+// }
+const templateString = (a, b) => `The sum is equal to ${(a + b).toString()}`;
+console.log("this returns a string");
--- InClass/A-ES6/exercise2.js
+++ InClass/A-ES6/exercise2.js
@@ -21,7 +21,7 @@ class Rectangle extends Polygon {
     this.sides = 4;
     //this.area = this.height * this.width;
   }
-  get area() {return this.width * this.height;}
+  get area() { return this.width * this.height; }
   sayName() {
     console.log('Hi I am a polygon and my name is ' + this.name + '.');
   }
@@ -32,8 +32,45 @@ r.sayName();
 //r.area = 3;
 console.log('The area of this polygon is ' + r.area);
 
+
+
 //Create 2 new classes that extend from polygon: triangle and circle and log their names and areas
 
+// class Tringle
+class Triangle extends Polygon {
+  constructor(height, width) {
+    super(height, width);
+    this.name = "Tringle";
+    this.sides = 3;
+  }
+  get area() {
+    return this.width * this.height / 2
+  } // 10 * 10 / 2 = 50 => 'The area of this polygon is  50'
+  sayName() {
+    console.log('Hi I am a polygon and my name is ' + this.name + '.');
+  }
 
+}
 
+const tringle = new Triangle(10, 10);
+tringle.sayName();
+console.log('The area of this polygon is ' + tringle.area);
+
+// class Circle
+
+class Circle extends Polygon {
+  constructor(height) {
+    super(height); // calls the parent constructor
+    this.name = "Circle";
+  }
+  get area() {
+    return (this.height * this.height * Math.PI) // PI =3.14... S = PI * r2
+  }
+  sayName() {
+    console.log('Hi I am a polygon and my name is ' + this.name + '.');
+  }
+}
+const circle = new Circle(20)
+circle.sayName()
+console.log('The area of this polygon is ' + circle.area);
 
